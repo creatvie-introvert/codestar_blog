a9b54a24fc98c5d802bf9bbb1bcc249cb98889f0
urbanIntrovert <trefoil72@gmail.com>
2025-12-01 11:53:04 +0000

    feat: add blog URL routing, class-based PostList view, templates, and update settings for deployment

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,14 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    field_2 = models.CharField(default='Hello, World!')
+    field_3 = models.CharField(null=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"The title of this post is {self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
@@ -27,4 +35,10 @@ class Comment(models.Model):
     )
     body = models.TextField()
     approved = models.BooleanField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)   
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
